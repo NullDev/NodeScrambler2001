fec0ff37b3e2d5bfd2e9f2d5d6f1757c6373c3e8
NullDev (Shadow) <NL-Dev@yandex.com>
2018-05-17 13:45:45 +0200

    did stuff

--- utils/scrambler.js
+++ utils/scrambler.js
@@ -6,7 +6,9 @@
 //----------------------------//
 ////////////////////////////////
 
-const alphabetA = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .,!?§$%&/()=-_:;";
+let path = require("path");
+
+const alphabetA = require(__dirname + "/../data/alphabet")[0];
 
 class Scrambler {
 
@@ -23,29 +25,43 @@ class Scrambler {
 
     encrypt(msg){
         let res = "";
-        for (let letter of msg){
-            let digest, letterVal = 0;
-            for (let i = 0; i < alphabetA.length; i++) if (letter == alphabetA.charAt(i)) letterVal = i;
-            digest = this.keyTxtVal.charAt((letterVal + this.initShift) % this.keyTxtVal.length);
-            this.addOffset(this.shiftVals);
-            res += digest;
-        }
+        for (let letter of msg) res += this.encryptLetter(letter);
         return res;
     };
 
     decrypt(msg){
         let res = "";
-        for (let letter of msg){
-            let digest, shiftChar = 0, letterVal = 0;
-            for (let i = 0; i < this.keyTxtVal.length; i++) if (letter == this.keyTxtVal.charAt(i)) letterVal = i;
-            shiftChar = letterVal - this.initShift;
-            while (shiftChar < 0) shiftChar += alphabetA.length;
-            digest = alphabetA.charAt(shiftChar);
-            this.addOffset(this.shiftVals);
-            res += digest;
-        }
+        for (let letter of msg) res += this.decryptLetter(letter);
         return res;
     };
+
+    encryptLetter(letter) {
+        let digest;
+        let letterVal = 0;
+
+        for (let i = 0; i < alphabetA.length; i++) if (letter == alphabetA.charAt(i)) letterVal = i;
+
+        digest = this.keyTxtVal.charAt((letterVal + this.initShift) % this.keyTxtVal.length);
+        this.addOffset(this.shiftVals);
+
+        return digest;
+    };
+
+    decryptLetter(letter) {
+        let digest;
+        let shiftChar = 0;
+        let letterVal = 0;
+
+        for (let i = 0; i < this.keyTxtVal.length; i++) if (letter == this.keyTxtVal.charAt(i)) letterVal = i;
+
+        shiftChar = letterVal - this.initShift;
+        while (shiftChar < 0) shiftChar += alphabetA.length;
+
+        digest = alphabetA.charAt(shiftChar);
+        this.addOffset(this.shiftVals);
+
+        return digest;
+    }
 }
 
 module.exports = Scrambler;
